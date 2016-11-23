ba9e7c510161ae313c6fbbdc2abb5c23e72eaecd
raymond <782356165@qq.com>
2016-11-23 16:33:33 +0800

    修改图片

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,7 @@ import React from 'react';
 
 //拿取images文件夹中的json
 let imageDatas = require('../data/imageDatas.json');
-let yeomanImage = require('../images/yeoman.png');
+// let yeomanImage = require('../images/yeoman.png');
 
 //为json文件添加每个图片的url
 imageDatas = (function getImageUrl(imageDatas) {
@@ -19,12 +19,38 @@ imageDatas = (function getImageUrl(imageDatas) {
 })(imageDatas);
 
 
-class AppComponent extends React.Component {
+class ImgFigure extends React.Component {
   render() {
     return (
+      <figure className="img-figure">
+        <img src={this.props.data.url} alt={this.props.data.title}/>
+        <figcaption>
+          <h2 className="img-title">{this.props.data.title}</h2>
+        </figcaption>
+      </figure>
+    );
+  }
+}
+
+
+class AppComponent extends React.Component {
+  render() {
+
+    let controllerUnits = [],
+        imgFigures = [];
+
+    imageDatas.forEach(function (value, index) {
+      imgFigures.push(<ImgFigure data={value} key={index} />);
+    });
+
+    return (
       <section className="stage">
-        <section className="img-sec"></section>
-        <nav className="controller-nav"></nav>
+        <section className="img-sec">
+          {imgFigures}
+        </section>
+        <nav className="controller-nav">
+          {controllerUnits}
+        </nav>
       </section>
     );
   }
